98f702c70098184b5dc3257c2f6f491f5f3d7868
Alex Linden <alextl2004@gmail.com>
2025-03-04 22:42:06 -0800

    more learning

--- src/Main.kt
+++ src/Main.kt
@@ -19,6 +19,28 @@ fun sum(x: Int, y: Int): Int {                                          // 3
 fun multiply(x: Int, y: Int) = x * y                                    // 4
 
 fun main() {
+
+    var neverNull: String? = "This can't be null"            // 1
+
+    neverNull = null                                        // 2
+
+    var nullable: String? = "You can keep a null here"      // 3
+
+    nullable = null                                         // 4
+
+    var inferredNonNull = "The compiler assumes non-null"   // 5
+
+    inferredNonNull = null                                  // 6
+
+    fun strLength(notNull: String): Int {                   // 7
+        return notNull.length
+    }
+
+    strLength(neverNull)                                    // 8
+    strLength(nullable)
+
+
+
     fun printAll(vararg messages: String){
         for (m in messages) println(m)
     }
